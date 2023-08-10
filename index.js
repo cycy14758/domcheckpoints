9a21a8b9f3b0586b9ccb3059bd95a1507fbb2784
cycy14758 <Cyrinebentaazayet2@gmail.com>
2023-08-10 21:56:20 +0100

    commit

--- index.js
+++ index.js
@@ -1,69 +1,68 @@
-var btn1plus=document.getElementsByClassName("btn1+")
- console.log(btn1plus)
-  var   btn1moin=document.getElementsByClassName("btn1-")
-  console.log(btn1moin)
-  var c1=document.getElementsByClassName("c1")
-  console.log(c1)
-  var hearts=document.getElementsByClassName("fa-solid ")
-  console.log(hearts)
-  
-  var title=document.getElementsByClassName("card_title")
-  console.log(title)
+var btn1plus = document.getElementsByClassName("btn1+");
+console.log(btn1plus);
+var btn1moin = document.getElementsByClassName("btn1-");
+var c1 = document.getElementsByClassName("c1");
+console.log(c1);
+var hearts = document.getElementsByClassName("fa-solid");
+var title = document.getElementsByClassName("card_title");
+var prt = document.getElementById("parent");
+var prx = document.getElementsByClassName("prix");
+var total = document.getElementById("total");
 
+var somme = 0;
 
-
-
-
-  var parent=document.getElementById("parent")
-  console.log(parent)
-
-  var newDiv = document.createElement('li');
-  var newContent = document.createTextNode(title);
-  newDiv.appendChild(newContent); 
-  document.body.appendChild(newDiv, parent)
-
-
-
-
-
-
-
-
-  for ( let i=0 ; i < hearts.length ; i++){
-    hearts[i].addEventListener("click",function(){
-if (hearts[i].style.color=="black"  )
-{
-hearts[i].style.color="red"  
-}        
-else 
-hearts[i].style.color="black"  
+for (let i = 0; i < hearts.length; i++) {
+  hearts[i].addEventListener("click", function () {
+    if (hearts[i].style.color === "black") {
+      hearts[i].style.color = "red";
+    } else {
+      hearts[i].style.color = "black";
+    }
+  });
 }
-  )}
+
+for (let i = 0; i < btn1plus.length; i++) {
+  let compteur1 = 0;
+
+  btn1plus[i].addEventListener("click", function () {
+    compteur1 += 1;
+    c1[i].innerHTML = compteur1;
+
+    somme += parseInt(prx[i].innerHTML); 
+
+    total.innerHTML = somme;
+
+    if (compteur1 > 0) {
+      var newDiv = document.createElement("li");
+      var newContent = document.createTextNode(title[i].textContent);
+      newDiv.appendChild(newContent);
+
+      var removeButton = document.createElement("button");
+      removeButton.textContent = "x";
+      removeButton.addEventListener("click", function () {
+        prt.removeChild(newDiv);
 
 
-  for ( let i=0 ; i < btn1plus.length ; i++){
-    let compteur1=0;
-  btn1plus[i].addEventListener("click", function ()  { 
-    compteur1 += 1 ;
-     c1[i].innerHTML = compteur1;
-     console.log( c1[i]);
-    })
+        
+        somme -= parseInt(prx[i].innerHTML);
 
-          btn1moin[i].addEventListener("click", function ()   { 
-            if (c1[i].innerHTML>0 ) 
-        {    c1[i].innerHTML = compteur1;
-            compteur1-= 1;
-          ;}
-            return  compteur1
-          })
-          if (c1[i].innerHTML>0) {
-            var newContent = document.createTextNode(title[i])
-            newDiv.appendChild(newContent); 
-       document.body.appendChild(newDiv, parent)
+        total.innerHTML = somme;
+        compteur1 = 0;
+        c1[i].innerHTML = compteur1;
+      });
 
-          } 
-        }
-    
+      newDiv.appendChild(removeButton);
+      prt.appendChild(newDiv);
+    }
+  });
 
+  btn1moin[i].addEventListener("click", function () {
+    if (compteur1 > 0) {
+      compteur1 -= 1;
+      c1[i].innerHTML = compteur1;
 
-         
+      somme -= parseInt(prx[i].innerHTML); 
+      total.innerHTML = somme;
+    }
+  });
+}
